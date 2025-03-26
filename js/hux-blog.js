c392f6d8bd355ca7a3adecb7d192bac6c76c4965
Rancho <ranchocooper@gmail.com>
2025-03-27 03:40:04 +0800

    update go-hexagonal post

--- js/hux-blog.js
+++ js/hux-blog.js
@@ -5,7 +5,7 @@
  */
 
 // Tooltip Init
-$(function() {
+$(function () {
     $("[data-toggle='tooltip']").tooltip();
 });
 
@@ -21,43 +21,50 @@ $(function() {
 // });
 
 // responsive tables
-$(document).ready(function() {
-	$("table").wrap("<div class='table-responsive'></div>");
-	$("table").addClass("table");
+$(document).ready(function () {
+    $("table").wrap("<div class='table-responsive'></div>");
+    $("table").addClass("table");
 });
 
 // responsive embed videos
-$(document).ready(function () { 
+$(document).ready(function () {
     $('iframe[src*="youtube.com"]').wrap('<div class="embed-responsive embed-responsive-16by9"></div>');
-	$('iframe[src*="youtube.com"]').addClass('embed-responsive-item');
+    $('iframe[src*="youtube.com"]').addClass('embed-responsive-item');
     $('iframe[src*="vimeo.com"]').wrap('<div class="embed-responsive embed-responsive-16by9"></div>');
-	$('iframe[src*="vimeo.com"]').addClass('embed-responsive-item');
+    $('iframe[src*="vimeo.com"]').addClass('embed-responsive-item');
 });
 
 // Navigation Scripts to Show Header on Scroll-Up
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
     var MQL = 1170;
 
     //primary navigation slide-in effect
     if ($(window).width() > MQL) {
         var headerHeight = $('.navbar-custom').height();
         $(window).on('scroll', {
-                previousTop: 0
-            },
-            function() {
+            previousTop: 0
+        },
+            function () {
                 var currentTop = $(window).scrollTop();
                 //check if user is scrolling up
                 if (currentTop < this.previousTop) {
                     //if scrolling up...
                     if (currentTop > 0 && $('.navbar-custom').hasClass('is-fixed')) {
                         $('.navbar-custom').addClass('is-visible');
+                        $('.navbar-custom').removeClass('is-hidden');
                     } else {
-                        $('.navbar-custom').removeClass('is-visible is-fixed');
+                        $('.navbar-custom').removeClass('is-visible is-fixed is-hidden');
                     }
                 } else {
                     //if scrolling down...
                     $('.navbar-custom').removeClass('is-visible');
-                    if (currentTop > headerHeight && !$('.navbar-custom').hasClass('is-fixed')) $('.navbar-custom').addClass('is-fixed');
+                    if (currentTop > headerHeight && !$('.navbar-custom').hasClass('is-fixed')) {
+                        $('.navbar-custom').addClass('is-fixed');
+                    }
+                    // 向下滚动超过头部高度时，添加隐藏类
+                    if (currentTop > headerHeight) {
+                        $('.navbar-custom').addClass('is-hidden');
+                    }
                 }
                 this.previousTop = currentTop;
             });
